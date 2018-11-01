c2c02ba2c0aea083073b2f48b7cd8cc86203a158
Roman Dawydkin <tanmatra@gmail.com>
2018-11-01 17:01:53 +0300

    Создание скриншотов

--- src/main/java/camera/CameraView.java
+++ src/main/java/camera/CameraView.java
@@ -1,9 +1,13 @@
 package camera;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Optional;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.geometry.Bounds;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
@@ -14,6 +18,7 @@ import javafx.scene.control.Separator;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TitledPane;
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -21,12 +26,17 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
 import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
 class CameraView extends TitledPane implements Camera
 {
     private static final FileChooser.ExtensionFilter ALL_FILES = new FileChooser.ExtensionFilter("All files", "*.*");
 
     private static final FileChooser.ExtensionFilter MP4_FILES = new FileChooser.ExtensionFilter("MP4", "*.mp4");
+
+    private static final FileChooser.ExtensionFilter PNG_FILES = new FileChooser.ExtensionFilter("PNG", "*.png");
+
+    private static final FileChooser.ExtensionFilter JPG_FILES = new FileChooser.ExtensionFilter("JPG", "*.jpg");
 
     private final Configuration configuration;
 
@@ -80,17 +90,39 @@ class CameraView extends TitledPane implements Camera
         final Button stopButton = new Button("Stop");
         stopButton.setOnAction(event -> stop());
 
+        final Button screenshotButton = new Button("Screenshot");
+        screenshotButton.setOnAction(event -> saveScreenshotToFile());
+
         final HBox buttonsPanel = new HBox(
                 openURIButton,
                 openFileButton,
                 new Separator(Orientation.VERTICAL),
                 playButton,
-                stopButton);
+                stopButton,
+                new Separator(Orientation.VERTICAL),
+                screenshotButton);
         buttonsPanel.setAlignment(Pos.CENTER_LEFT);
         buttonsPanel.setPadding(new Insets(5.0));
         buttonsPanel.setSpacing(5.0);
 
         return buttonsPanel;
+    }
+
+    private void saveScreenshotToFile() {
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(PNG_FILES, JPG_FILES, ALL_FILES);
+        final File file = fileChooser.showSaveDialog(getScene().getWindow());
+        if (file == null) {
+            return;
+        }
+        try {
+            makeScreenshot(file.toString());
+        } catch (Exception e) {
+            final Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("Error saving screenshot");
+            alert.setContentText(e.toString());
+            alert.showAndWait();
+        }
     }
 
     private static File uriToFile(String uriString) {
@@ -202,11 +234,28 @@ class CameraView extends TitledPane implements Camera
 
     @Override
     public Image getScreenshot() {
-        return null; //TODO
+        final Bounds bounds = mediaView.getBoundsInLocal();
+        final WritableImage image = new WritableImage((int) bounds.getWidth(), (int) bounds.getHeight());
+        return mediaView.snapshot(null, image);
     }
 
     @Override
     public void makeScreenshot(String path) {
-        //TODO
+        final int dotIndex = path.lastIndexOf('.');
+        if (dotIndex < 0) {
+            throw new RuntimeException("Cannot determine image file format");
+        }
+        final String format = path.substring(dotIndex + 1);
+        final Image screenshot = getScreenshot();
+        final BufferedImage bufferedImage = SwingFXUtils.fromFXImage(screenshot, null);
+        final boolean saved;
+        try {
+            saved = ImageIO.write(bufferedImage, format, new File(path));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (!saved) {
+            throw new RuntimeException("Unsupported image writer");
+        }
     }
 }
